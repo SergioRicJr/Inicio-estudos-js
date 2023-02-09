ac0c1e1602389f33215d5f64540e862555dac97a
Sergio <sergioricjr7@gmail.com>
2023-02-08 22:15:08 -0300

    comecando estudos em js

---
+++ index.js
@@ -0,0 +1,21 @@
+console.log('eae');
+
+n1 = 6;
+n2 = 5;
+soma= n1*n2;
+
+// soma = 'agr essa é a variavel'
+
+
+console.log(soma);
+console.log(typeof(soma));
+
+// window.alert('vai explodir')
+// window.confirm('vai querer entrar?')
+
+var nome = window.prompt("digite seu nome: ")
+// var idade = window.prompt("digite sua idade: ")
+var mensagem = `Bem vindo ${nome}`
+console.log(mensagem)
+window.alert(mensagem)
+

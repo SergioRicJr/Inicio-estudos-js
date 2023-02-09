17f0d99b4bc2bb31bc1bb8a598b8713e98442c02
Sergio <sergioricjr7@gmail.com>
2023-02-08 22:45:10 -0300

    estudos js 2

--- index.js
+++ index.js
@@ -15,7 +15,15 @@ console.log(typeof(soma));
 
 var nome = window.prompt("digite seu nome: ")
 // var idade = window.prompt("digite sua idade: ")
-var mensagem = `Bem vindo ${nome}`
+var mensagem = `Bem vindo ${nome}` //formatacao no js
 console.log(mensagem)
 window.alert(mensagem)
 
+//necessario definir como tipo number nesse caso
+//widow.prompt sempre retorna string
+//casting ou parse == transformacao de tipo
+//da pra deixar so number
+var numero1 = Number.parseInt(window.prompt("digite o primeiro numero: "))
+var numero2 = Number.parseInt(window.prompt("Digite o segundo numero: "))
+var soma = numero1 + numero2
+window.alert(soma)
